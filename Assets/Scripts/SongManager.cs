ed9f8cdbd1cf68ec544ea9416c614fd9e83c4c6d
prashanthPadiachy <prashanth.padiachy@gmail.com>
2026-02-11 23:34:01 +0200

    Initial commit

---
+++ Assets/Scripts/SongManager.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+using System.IO;
+using UnityEngine.Networking;
+using System;
+public class SongManager : MonoBehaviour
+{
+    public static SongManager Instance;
+    public AudioSource audioSource;
+    public float songDelayinSecs;
+    public int inputDeleyinMs;
+
+    public string fileLocation;
+    public float noteTime;
+    public float noteSpawnY;//where note instantiates
+    public float noteTapY;//where it should be tapped
+    public float noteDespawnY
+    {
+        get 
+        {
+            return noteTapY - (noteSpawnY - noteTapY);
+        }
+    }
+
+    public static MidiFile midiFile;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Instance = this;
+
+        if (Application.streamingAssetsPath.StartsWith("Http"))
+        {
+            StartCoroutine(ReadFromWebsite());
+        }
+        else 
+        {
+            ReadFromFile();
+        }
+    }
+
+    private void ReadFromFile()
+    {
+        MidiFile.Read(Application.streamingAssetsPath+"/"+fileLocation);
+        GetDataFromMidi();
+    }
+
+    private IEnumerator ReadFromWebsite()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get(Application.streamingAssetsPath + "/" + fileLocation)) 
+        {
+            yield return www.SendWebRequest();
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(www.error);
+            }
+            else 
+            {
+                byte[] results = www.downloadHandler.data;
+                using (var stream = new MemoryStream(results))
+                { 
+                    midiFile = MidiFile.Read(stream);
+                    GetDataFromMidi();
+                }
+            }
+        }
+    }
+
+    public void GetDataFromMidi()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# RhythmTest
+
